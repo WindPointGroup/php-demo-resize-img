2706a4066926a54ef4bf3a87e0adea7eb43e6240
John Binzak <john.binzak@gmail.com>
2020-04-22 07:45:27 -0500

    ###Reusable Resize Function Let's first create a simple php file called `index.php` for demo purposes. Then let's create a function called `createResizedImage` which intakes 2 arguments. The first being a string `$image url`. The second being an optional int `$max dimension`. This will be our reusable resize function. It will return an image resource upon success, or the false boolean if an error occurs. [SNIPPET]

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: johnbinzak
+ * Date: 2020-04-22
+ * Time: 07:28
+ */
+
+// [SNIPPET]
+/**
+ * @param $image_url
+ * @param int $max_dimension
+ * @return bool|resource
+ */
+function createResizedImage($image_url, $max_dimension = 200) {
+
+}
+// [/SNIPPET]
